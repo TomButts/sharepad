a35f7d7d11a83e3f0899656ff1dd485da65622d6
Tom Butts <tom.butts93@gmail.com>
2024-04-08 11:51:22 +0100

    Change baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://127.0.0.1:8080',
   },
 })
